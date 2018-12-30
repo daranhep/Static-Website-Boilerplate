a7fb2a4e32e62929209de977001b55857160d941
Dara Nhep <dara.h.nhep@gmail.com>
2018-12-29 19:31:38 -0600

    Added dev server

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-console.log(`I've been required by Webpack`);
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+(() => {
+  const IAmES6 = `I must be transpiled by Babel`;
+  console.log(IAmES6);
+})();
 
 import "./_scss/main.scss";
